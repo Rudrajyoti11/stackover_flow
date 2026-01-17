21037b2a38cc24da30cfa0f1ec1d3ce2152ec459
Rudrajyoti11 <92710279+Rudrajyoti11@users.noreply.github.com>
2026-01-18 00:32:52 +0530

    setup Auth Layout,Social Auth Form,Next Auth,Oauth with Google and Github Providers

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const sign_in = () => {
+  return <div>Sign in</div>;
+};
+
+export default sign_in;
---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const signup = () => {
+  return <div>Sign up</div>;
+};
+
+export default signup;
