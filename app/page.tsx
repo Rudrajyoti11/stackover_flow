f58d54e655d58508633b472b231795186c0a1b9d
Rudrajyoti11 <92710279+Rudrajyoti11@users.noreply.github.com>
2026-01-17 18:09:13 +0530

    Setup Light and Dark Mode

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,27 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
 const page = () => {
   return (
     <div>
-      <h1 className="text-2xl font-bold text-blue-400">Welcome to the Home Page</h1>
+      <h1 className="h1-bold">Welcome to the Home Page</h1>
+      <DropdownMenu>
+        <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>Profile</DropdownMenuItem>
+          <DropdownMenuItem>Billing</DropdownMenuItem>
+          <DropdownMenuItem>Team</DropdownMenuItem>
+          <DropdownMenuItem>Subscription</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </div>
   );
 };
